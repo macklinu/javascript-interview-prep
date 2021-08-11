d81a55137d09195ca66a4ccc8ab1486e1e6650cb
mackie <macklinu@users.noreply.github.com>
2021-08-11 09:25:59 -0400

    chore: add plop file generator

---
+++ plopfile.js
@@ -0,0 +1,32 @@
+/**
+ * @param {import('plop').NodePlopAPI} plop
+ */
+module.exports = function (plop) {
+  plop.setGenerator('problem', {
+    description: 'interview problem boilerplate',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'problem name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/{{dashCase name}}/index.ts',
+        templateFile: 'templates/index.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/{{dashCase name}}/index.test.ts',
+        templateFile: 'templates/index.test.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/{{dashCase name}}/README.md',
+        templateFile: 'templates/README.hbs',
+      },
+    ],
+  })
+}
