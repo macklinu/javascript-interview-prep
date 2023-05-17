59bbd2fdfc9ae9de26ebd22f5637af8c79b43ea0
mackie <macklinu@users.noreply.github.com>
2023-05-17 15:32:59 -0400

    refactor(flatten-array): use generic type

--- src/flatten-array/index.ts
+++ src/flatten-array/index.ts
@@ -1,7 +1,7 @@
-export type NestedArray = Array<number | NestedArray>
+export type NestedArray<T> = Array<T | NestedArray<T>>
 
-export function flattenArray(array: NestedArray): number[] {
-  const result: number[] = []
+export function flattenArray<T>(array: NestedArray<T>): T[] {
+  const result: T[] = []
   for (const value of array) {
     if (Array.isArray(value)) {
       result.push(...flattenArray(value))
